a0a455edf0dd8cd5d354be5db84ba252706ecc3c
Garrett Goon <garrett.goon@hpe.com>
2023-08-02 11:35:41 -0400

    add greedy generation test

---
+++ python/greedy_generate.py
@@ -0,0 +1,32 @@
+import torch
+from decoder_only import DecoderOnly
+from defaults import B
+
+
+class DecoderOnlyGreedyNoCache(DecoderOnly):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def generate(self, inputs, max_length):
+        """Naive, minimal generation method. Assumes inputs are already tokenized."""
+        assert max_length <= self.block_size
+        self.eval()
+        outputs = inputs.clone()
+        while outputs.shape[1] < max_length:
+            context = outputs[:, -self.block_size :]
+            last_token_pred_logits = self(context)[:, -1]
+            most_probable_token = last_token_pred_logits.argmax(dim=-1)[:, None]
+            outputs = torch.cat([outputs, most_probable_token], dim=-1)
+        return outputs
+
+
+def test_generation():
+    model = DecoderOnlyGreedyNoCache()
+    inputs = torch.randint(high=model.vocab_size, size=(B, model.block_size // 2))
+    outputs = model.generate(inputs, model.block_size)
+    assert outputs.shape == torch.Size([B, model.block_size])
+    assert outputs.max() <= model.vocab_size
+
+
+if __name__ == "__main__":
+    test_generation()
